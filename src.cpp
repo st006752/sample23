fcc0028cf0d7de1ee47ad144a1f08b3c6af9a515
Коровкин Максим Васильевич <st006752@ad.pu.ru>
2023-10-17 11:49:10 +0300

    Added var check

--- src.cpp
+++ src.cpp
@@ -2,5 +2,10 @@
 int main(){
     std::cout << "Hello, World!" << std::endl;
     int var = 0;
+    std::cin >> var;
+    if ( var < 0 ) {
+        std::cout << "Bad input" << std::endl;
+	return 1;
+    }
     return 0;
 }
